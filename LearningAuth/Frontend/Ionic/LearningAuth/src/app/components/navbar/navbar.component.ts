61d3d11c55acc8d257722e04f0a9a8016bf37246
Sandro Spengler <sandro.spengler@web.de>
2021-01-31 15:07:01 +0100

    Added Buttons for Login and Logout

--- LearningAuth/Frontend/Ionic/LearningAuth/src/app/components/navbar/navbar.component.ts
+++ LearningAuth/Frontend/Ionic/LearningAuth/src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {
+    this.checkForLogin();
+  }
 
-  ngOnInit() {}
+  checkForLogin = (): boolean => {
+    if (sessionStorage.getItem("token")) {
+      console.log("true");
+      return true;
+    } else {
+      return false;
+    }
+  };
 
+  logout = (): void => {
+    sessionStorage.removeItem("token");
+  };
 }
